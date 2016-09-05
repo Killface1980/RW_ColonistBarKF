e8e4cb4ff32d29f4d1e76a5946100ce5c0ce0a6f
Killface1980 <tmgaertner@googlemail.com>
2016-09-05 12:14:03 +0200

    added injector

--- Source/RW_ColonistBarKF/MapComponentInjectorBehavior.cs
+++ Source/RW_ColonistBarKF/MapComponentInjectorBehavior.cs
@@ -8,7 +8,9 @@ namespace RW_ColonistBarKF       // Replace with yours.
     public class MapComponentInjectorBehavior : MonoBehaviour
     {
         public static readonly string mapComponentName = "RW_ColonistBarKF.MapComponent_FollowMe";       // Ditto.
-        private static readonly RW_ColonistBarKF.MapComponent_FollowMe mapComponent = new RW_ColonistBarKF.MapComponent_FollowMe();       // Ditto.
+        private static readonly MapComponent_FollowMe mapComponent = new MapComponent_FollowMe();       // Ditto.
+        public static readonly string mapComponentName2 = "RW_ColonistBarKF.MapComponent_ZoomToMouse";       // Ditto.
+        private static readonly MapComponent_ZoomToMouse mapComponent2 = new MapComponent_ZoomToMouse();       // Ditto.
 
         #region No editing required
         protected bool reinjectNeeded = false;
@@ -36,7 +38,7 @@ namespace RW_ColonistBarKF       // Replace with yours.
 
             if (reinjectNeeded)
             {
-                Log.Message("FollowMeInjector - needs reinject");
+                Log.Message("ColonistBarKFInjector - needs reinject");
                 reinjectTime -= Time.fixedDeltaTime;
                 if (reinjectTime <= 0)
                 {
@@ -56,6 +58,18 @@ namespace RW_ColonistBarKF       // Replace with yours.
                             Log.Message("MapComponentInjector: success!");
                             //Destroy(gameObject);
                         }
+                        if (Find.Map.components.FindAll(x => x.GetType().ToString() == mapComponentName2).Count != 0)
+                        {
+                            Log.Message("MapComponentInjector: map already has " + mapComponentName2 + ".");
+                            //Destroy(gameObject);
+                        }
+                        else
+                        {
+                            Log.Message("MapComponentInjector: adding " + mapComponentName2 + "...");
+                            Find.Map.components.Add(mapComponent2);
+                            Log.Message("MapComponentInjector: success!");
+                            //Destroy(gameObject);
+                        }
                     }
                 }
             }
@@ -69,14 +83,14 @@ namespace RW_ColonistBarKF       // Replace with yours.
 
     class MapComponentInjector : ITab
     {
-        protected UnityEngine.GameObject initializer;
+        protected GameObject initializer;
 
         public MapComponentInjector()
         {
-            Log.Message("MapComponentInjector: initializing for " + MapComponentInjectorBehavior.mapComponentName);
-            initializer = new UnityEngine.GameObject("MapComponentInjector");
+            Log.Message("MapComponentInjector: initializing for " + MapComponentInjectorBehavior.mapComponentName + " and " + MapComponentInjectorBehavior.mapComponentName);
+            initializer = new GameObject("CBKFMapComponentInjector");
             initializer.AddComponent<MapComponentInjectorBehavior>();
-            UnityEngine.Object.DontDestroyOnLoad((UnityEngine.Object)initializer);
+            Object.DontDestroyOnLoad((Object)initializer);
         }
 
         protected override void FillTab()
---
+++ Source/RW_ColonistBarKF/MapComponent_ZoomToMouse.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace RW_ColonistBarKF
+{
+    public class MapComponent_ZoomToMouse : MapComponent
+    {
+        // backing private fields / properties
+        private const BindingFlags all = (BindingFlags)60;
+        private FieldInfo curSize_FI = typeof(CameraDriver).GetField("rootSize", all);
+        private FieldInfo desSize_FI = typeof(CameraDriver).GetField("desiredSize", all);
+        private MethodInfo curPos = typeof(CameraDriver).GetProperty("CurrentRealPosition", all).GetGetMethod(true);
+
+        // tolerance for zoom
+        private float tolerance = .1f;
+
+        // helpers
+        private Vector3 LastMouseMapPosition = Vector3.zero;
+        private Vector3 CurrentMouseMapPosition => Gen.MouseMapPosVector3();
+        private Vector3 MouseMapOffset => LastMouseMapPosition - CurrentMouseMapPosition;
+
+        // reflection helpers
+        public float CurrentSize => (float)curSize_FI.GetValue(Current.CameraDriver);
+        public float DesiredSize => (float)desSize_FI.GetValue(Current.CameraDriver);
+        public Vector3 CurrentRealPosition => (Vector3)curPos.Invoke(Current.CameraDriver, null);
+
+        public override void MapComponentUpdate()
+        {
+            // determine zoom action
+            float action = CurrentSize - DesiredSize;
+
+            // zoom action has taken place
+            if (action > tolerance)
+                Current.CameraDriver.JumpTo(CurrentRealPosition + MouseMapOffset);
+            else
+                // update last known location.
+                LastMouseMapPosition = CurrentMouseMapPosition;
+
+            // NOTE: Ideally, we'ld like to do this within the zooming code. I've been unable to get access without causing errors (detours + loads of reflection).
+            // the net result is the current simple but slightly wonky behavious. Movement of the map is a bit jittery, and moving the mouse during scroll moves the map directly.
+        }
+    }
+}
