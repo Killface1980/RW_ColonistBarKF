de8f21dc9ef413e9b5b04e5ec0bea214bbe3eb20
Killface1980 <tmgaertner@googlemail.com>
2017-01-08 10:08:34 +0100

    added blank mapcomponents to avoid bogus bug reports

---
+++ Source/RW_ColonistBarKF/OLD_A16/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace ColonistBarKF
+{
+    public class MapComponent_FollowMe : MapComponent
+    {
+        public MapComponent_FollowMe(Map map) : base(map)
+        {
+        }
+    }
+    public class MapComponent_ZoomToMouse : MapComponent
+    {
+        public MapComponent_ZoomToMouse(Map map) : base(map)
+        {
+        }
+    }
+
+}
